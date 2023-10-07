ed77a2bd842dd553ffcb1f206abf135bfb0ed7fc
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-10-07 18:28:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from pyrogram import filters, Client, idle
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+import openai
+
+API = "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"
+openai.api_key = API
+
+API_ID=14688437
+API_HASH="5310285db722d1dceb128b88772d53a6"
+BOT_TOKEN="6652935072:AAEDRvQfbuQVdxpOpillomYwpYn6euetpdY"
+
+studygpt = Client("studygpt", api_id=API_ID,api_hash=API_HASH,bot_token=BOT_TOKEN)
+
+@studygpt.on_message(filters.command("start"))
+async def start(studygpt,message):
+    await message.reply_text(
+         text="""📚 Wᴇʟᴄᴏᴍᴇ ᴛᴏ SᴛᴜᴅʏBᴏᴛ! 🤖
+
+I'ᴍ ʜᴇʀᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴡɪᴛʜ ʏᴏᴜʀ sᴛᴜᴅɪᴇs. Jᴜsᴛ ᴛʏᴘᴇ ʏᴏᴜʀ ᴏ̨ᴜᴇsᴛɪᴏɴs ᴏʀ ᴛᴏᴘɪᴄs, ᴀɴᴅ I'ʟʟ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜ ᴡɪᴛʜ ᴇxᴘʟᴀɴᴀᴛɪᴏɴs, sᴜᴍᴍᴀʀɪᴇs, ᴀɴᴅ ᴀɴsᴡᴇʀs. Lᴇᴛ's ʟᴇᴀʀɴ ᴛᴏɢᴇᴛʜᴇʀ!
+
+Tʏᴘᴇ '/help' ᴛᴏ sᴇᴇ ᴀ ʟɪsᴛ ᴏғ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs.
+
+Hᴀᴘᴘʏ sᴛᴜᴅʏɪɴɢ! 📖✨""",
+          reply_markup=start_keyboard,
+    )
+
+studygpt.run()
+
