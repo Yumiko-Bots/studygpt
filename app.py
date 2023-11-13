22e5288bc69d6a5dc84fadfc59b651a0f73db20b
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-11-13 16:10:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from pyrogram import filters, Client, idle
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import openai
 import asyncio
-from pyrogram.enums import ChatAction
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 API_ID = 14688437
 API_HASH = "5310285db722d1dceb128b88772d53a6"
@@ -12,7 +11,7 @@ BOT_TOKEN = "6652935072:AAEDRvQfbuQVdxpOpillomYwpYn6euetpdY"
 
 studygpt = Client("studygpt", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-def genrate_code(prompt):
+def generate_code(prompt):
     chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
     answer = chat_completion.choices[0].message["content"]
     return answer
@@ -21,25 +20,25 @@ def genrate_code(prompt):
 async def start(_, message):
     keyboard = [
         [
-            InlineKeyboardButton("Hᴇʟᴘ & Cᴏᴍᴍᴀɴᴅs", callback_data="help"),
-            InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ", url="https://t.me/yumiko_group"),
+            InlineKeyboardButton("Help & Commands", callback_data="help"),
+            InlineKeyboardButton("Support", url="https://t.me/yumiko_group"),
         ],
         [
-            InlineKeyboardButton("Uᴘᴅᴀᴛᴇs", url="https://t.me/yumikoupdates"),
-            InlineKeyboardButton("Aʙᴏᴜᴛ", callback_data="about"),
+            InlineKeyboardButton("Updates", url="https://t.me/yumikoupdates"),
+            InlineKeyboardButton("About", callback_data="about"),
         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     msg = await message.reply_text("Bot is starting....")
     await asyncio.sleep(0.3)
     await msg.edit_text(
-        text="""📚 Wᴇʟᴄᴏᴍᴇ ᴛᴏ Sᴛᴜᴅʏ ɢᴘᴛ Bᴏᴛ! 🤖
+        text="""📚 Welcome to Study GPT Bot! 🤖
 
-I'ᴍ ʜᴇʀᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴡɪᴛʜ ʏᴏᴜʀ sᴛᴜᴅɪᴇs. Jᴜsᴛ ᴛʏᴘᴇ ʏᴏᴜʀ ᴏ̨ᴜᴇsᴛɪᴏɴs ᴏʀ ᴛᴏᴘɪᴄs, ᴀɴᴅ I'ʟʟ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜ ᴡɪᴛʜ ᴇxᴘʟᴀɴᴀᴛɪᴏɴs, sᴜᴍᴍᴀʀɪᴇs, ᴀɴᴅ ᴀɴsᴡᴇʀs. Lᴇᴛ's ʟᴇᴀʀɴ ᴛᴏɢᴇᴛʜᴇʀ!
+I'm here to help you with your studies. Just type your questions or topics, and I'll provide you with explanations, summaries, and answers. Let's learn together!
 
-Tʏᴘᴇ '/help' ᴛᴏ sᴇᴇ ᴀ ʟɪsᴛ ᴏғ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs.
+Type '/help' to see a list of available commands.
 
-Hᴀᴘᴘʏ sᴛᴜᴅʏɪɴɢ! 📖✨
+Happy studying! 📖✨
 """,
         reply_markup=reply_markup,
     )
@@ -48,33 +47,33 @@ Hᴀᴘᴘʏ sᴛᴜᴅʏɪɴɢ! 📖✨
 async def help(_, message):
     keyboard = [
         [
-            InlineKeyboardButton("Cᴏᴅɪɴɢ Assɪsᴛᴀɴᴛ", callback_data="coding"),
+            InlineKeyboardButton("Coding Assistant", callback_data="coding"),
         ],
         [
-            InlineKeyboardButton("Iᴍᴀɢᴇ ᴛᴏ Tᴇxᴛ", callback_data="image_to_text"),
+            InlineKeyboardButton("Image to Text", callback_data="image_to_text"),
         ],
         [
-            InlineKeyboardButton("Cʜᴀᴛ GPT Assɪsᴛᴀɴᴛ", callback_data="chat_gpt"),
+            InlineKeyboardButton("Chat GPT Assistant", callback_data="chat_gpt"),
         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await message.reply_text("Hᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ sᴇᴄᴛɪᴏɴ ᴛᴏ sᴇᴇ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs:", reply_markup=reply_markup)
+    await message.reply_text("Here is the help section. Click the buttons below:", reply_markup=reply_markup)
 
 @studygpt.on_callback_query(filters.regex("help"))
 async def help_callback(_, query):
     keyboard = [
         [
-            InlineKeyboardButton("Cᴏᴅɪɴɢ Assɪsᴛᴀɴᴛ", callback_data="coding"),
+            InlineKeyboardButton("Coding Assistant", callback_data="coding"),
         ],
         [
-            InlineKeyboardButton("Iᴍᴀɢᴇ ᴛᴏ Tᴇxᴛ", callback_data="image_to_text"),
+            InlineKeyboardButton("Image to Text", callback_data="image_to_text"),
         ],
         [
-            InlineKeyboardButton("Cʜᴀᴛ GPT Assɪsᴛᴀɴᴛ", callback_data="chat_gpt"),
+            InlineKeyboardButton("Chat GPT Assistant", callback_data="chat_gpt"),
         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.message.reply_text("Hᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ sᴇᴄᴛɪᴏɴ ᴛᴏ sᴇᴇ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs:", reply_markup=reply_markup)
+    await query.message.reply_text("Here is the help section. Click the buttons below:", reply_markup=reply_markup)
 
 @studygpt.on_callback_query()
 async def callback_query_handler(_, query):
@@ -89,20 +88,19 @@ async def callback_query_handler(_, query):
 async def code(_, message):
     if len(message.text.split()) > 1:
         prompt = message.text.split(None, 1)[1]
-        answer = genrate_code(prompt)
+        answer = generate_code(prompt)
         await message.reply_text(answer)
     else:
         await message.reply_text("Please use /code (your code or ask a code)")
 
 @studygpt.on_message(filters.text)
 async def reply(_, message):
-    if (message.text).startswith("/"):
+    if message.text.startswith("/"):
         return
     else:
         prompt = message.text
-        answer = genrate_code(prompt)
+        answer = generate_code(prompt)
         await message.reply_text(answer)
-       
 
 studygpt.run()
 print("Bot started!")
